5a0d30f057826142024e81086f4d773a4802eb81
Matt <>
2018-03-03 23:02:34 -0500

    Vid 2,3: Added mongoose, created product schema model, created product seeder

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ var favicon = require("serve-favicon");
 var logger = require("morgan");
 var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
+var mongoose = require('mongoose');
 
 var index = require("./routes/index");
 
 var app = express();
+
+mongoose.connect('mongodb://localhost:27017/shopping');
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
