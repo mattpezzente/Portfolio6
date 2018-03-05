101d9f945ba79b651b71399d094577b43220cd12
Matt <>
2018-03-05 16:25:42 -0500

    Vid 6 - csurf + express-session installed, new routes added, csrf protection setup

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require("serve-favicon");
 var logger = require("morgan");
 var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
+var session = require('express-session');
 
 var index = require("./routes/index");
 
@@ -19,6 +20,7 @@ app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({secret: 'mysupersecret', resave: false, saveUninitialized: false}));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", index);
