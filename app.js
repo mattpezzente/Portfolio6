a5205ef42509e699146ab39fe8b02006d004ac90
Brennon Denny <brennondenny@Brennons-MacBook-Pro.local>
2018-03-18 15:20:01 -0400

    Complete Checkout Process

--- app.js
+++ app.js
@@ -10,16 +10,15 @@ var flash = require('connect-flash');
 var mongoose = require('mongoose');
 var expressHbs = require("express-handlebars");
 var validator = require('express-validator');
+var MongoStore = require('connect-mongo')(session);
 
 var routes = require('./routes/index');
 var userRoutes = require('./routes/user');
 
 var app = express();
 
-require('./config/passport');
-
 mongoose.connect('mongodb://localhost:27017/shopping');
-
+require('./config/passport');
 
 // view engine setup
 app.engine(".hbs", expressHbs({ defaultLayout: "layout", extname: ".hbs" }));
@@ -33,7 +32,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(validator());
 app.use(cookieParser());
-app.use(session({secret: 'mysupersecret', resave: false, saveUninitialized: false}));
+app.use(session({
+  secret: 'mysupersecret', 
+  resave: false, 
+  saveUninitialized: false,
+  store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  cookie: { maxAge: 180 * 60 * 1000 }
+}));
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
@@ -41,6 +46,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(function (req, res, next) {
   res.locals.login = req.isAuthenticated();
+  res.locals.session = req.session;
   next();
 });
 
